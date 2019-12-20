f045cab74b0912b41a301d842e5af11d8243f605
Administrator <Administrator@DELL>
2019-12-20 19:06:19 +0530

    Some more changes made.

--- SpringAOPWithAspectJ/src/main/java/com/javaLive/aop/TrackOp.java
+++ SpringAOPWithAspectJ/src/main/java/com/javaLive/aop/TrackOp.java
@@ -1,6 +1,7 @@
 package com.javaLive.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -13,6 +14,11 @@ public class TrackOp {
 
 	@Before("k()") // applying pointcut on before advice
 	public void myadvice(JoinPoint jp) {
-		System.out.println("additional concern");
+		System.out.println("additional concern before method call");
+	}
+	
+	@After("k()") // applying pointcut on before advice
+	public void myadvice1(JoinPoint jp) {
+		System.out.println("additional concern after method call");
 	}
 }
--- SpringAOPWithAspectJ/src/main/java/com/javaLive/main/MainClass.java
+++ SpringAOPWithAspectJ/src/main/java/com/javaLive/main/MainClass.java
@@ -9,12 +9,16 @@ public class MainClass {
 	public static void main(String[] args){
 		ApplicationContext context= new ClassPathXmlApplicationContext("spring-config.xml");
 		Operation e=(Operation)context.getBean("opBean");
+		System.out.println("====================================");
 		System.out.println("calling msg...");
 		e.msg();
+		System.out.println("====================================");
 		System.out.println("calling a...");
 		e.a();
+		System.out.println("====================================");
 		System.out.println("calling b...");
 		e.b();
+		System.out.println("====================================");
 		}
 	
 }
